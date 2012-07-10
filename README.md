794d62642a4b770c1d44e543d5742cc24f5763f8
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2012-07-10 16:44:12 +0300

    I added improved readme as well as sql setup script. In addition the pom.xml contains the required dependencies for the project.

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-rdbms-reduce
-============
+# rdbms-reduce
 
 This is a distributed reduce exercise for a scenario when resource ids are stored independently of resource details.
+
+## Databases setup
+
+To setup the test environment you must do the following:
+
+1. Create a user in your mysql server called provisioning.
+2. Execute the script sql/distributed_dbs.sql
+
+## Large volume of data
---
+++ sql/distributed_dbs.sql
@@ -0,0 +1,95 @@
+-- MySQL dump 10.13  Distrib 5.1.63, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: provisioning
+-- ------------------------------------------------------
+-- Server version	5.1.63-0ubuntu0.11.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `provisioning`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `provisioning` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `provisioning`;
+
+--
+-- Table structure for table `domains`
+--
+
+DROP TABLE IF EXISTS `domains`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `domains` (
+  `id` int(11) NOT NULL,
+  `prov_id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `description` longtext,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unq_domains_name` (`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `domains`
+--
+
+LOCK TABLES `domains` WRITE;
+/*!40000 ALTER TABLE `domains` DISABLE KEYS */;
+/*!40000 ALTER TABLE `domains` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `items`
+--
+
+DROP TABLE IF EXISTS `items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL,
+  `account_id` int(11) NOT NULL,
+  `prov_id` int(11) NOT NULL,
+  `prov_type` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unq_items_provitem` (`prov_id`,`prov_type`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `items`
+--
+
+LOCK TABLES `items` WRITE;
+/*!40000 ALTER TABLE `items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Current Database: `store1_products`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `store1_products` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `store1_products`;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-07-10 16:19:20
