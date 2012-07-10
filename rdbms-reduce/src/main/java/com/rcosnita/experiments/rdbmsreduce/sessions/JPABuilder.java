ad13c7d9cce0afa82225e646af056f8ae8ffad23
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2012-07-10 17:29:45 +0300

    I added cleanup procedure for opened entity manager factories.

--- rdbms-reduce/src/main/java/com/rcosnita/experiments/rdbmsreduce/sessions/JPABuilder.java
+++ rdbms-reduce/src/main/java/com/rcosnita/experiments/rdbmsreduce/sessions/JPABuilder.java
@@ -1,5 +1,9 @@
 package com.rcosnita.experiments.rdbmsreduce.sessions;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -12,15 +16,40 @@ import javax.persistence.Persistence;
  * @since 10.07.2012
  */
 public class JPABuilder {
+	private static final Map<String, EntityManagerFactory> managers = 
+			new HashMap<String, EntityManagerFactory>();
+	
 	/**
 	 * Method used to obtain an entity manager instance for a given persistence unit.
 	 *  
 	 * @param persistenceUnit The name of the persistence unit we want to obtain.
 	 * @return
 	 */
-	public static EntityManager getEntityManager(String persistenceUnit) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory(persistenceUnit);
+	public synchronized static EntityManager getEntityManager(String persistenceUnit) {
+		EntityManagerFactory emf = null;
+		
+		if(managers.containsKey(persistenceUnit)) {
+			emf = managers.get(persistenceUnit);
+		}
+		else {
+			emf = Persistence.createEntityManagerFactory(persistenceUnit);
+			managers.put(persistenceUnit, emf);
+		}
 			
 		return emf.createEntityManager();
+	}
+	
+	static {
+		/**
+		 * I add the cleanup of entity manager factories currently opened. 
+		 */
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				for(EntityManagerFactory emf : managers.values()) {
+					emf.close();
+				}
+			}
+		});
 	}
 }
